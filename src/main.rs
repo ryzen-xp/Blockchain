537be31d695a40e358c44de53dc510d60cfd71c3
Ryzen XP <iron4540@gmail.com>
2025-02-08 17:51:11 +0530

    delete

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-
-// fn main(){
-//     // let  s = String::from("Hello  wolrd  ");
-//   let  length = calculate_length();
-
-// println!("{}" , length);
-// }
-  
-//   // fn calculate_length( s: &String)->usize {
-//   //   s.len()
-//   // }
-//   fn calculate_length()-> &String {
-//     let s = String::from("hello there it me baby");
-//     s
-//   }
-// #![allow(unused)]
-// fn main() {
-//     use std::panic;
-    
-//     let result = panic::catch_unwind(|| {
-//         println!("hello!");
-//     });
-//     assert!(result.is_ok());
-    
-//     let result = panic::catch_unwind(|| {
-//         panic!("oh no!");
-//     });
-//     assert!(result.is_err());
-
-//     let  a : i32  = 12312;
- 
-// }
-// let  res = rest(a)-> i32{
-//     return a*22 ;
-// }
+fn main(){
+  print!("sandeep chauhan is here  for servise ");
+}
